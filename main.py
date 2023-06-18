269fd5b0f131b0f1ef36eb47fdd106e31cd74a9d
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2023-06-18 17:09:23 +0300

    Edestakas sivuissa

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from tkinter import *
 import tkinter as tk
 from bs4 import BeautifulSoup
 
+# Edellisen sivun vaihto
+es = []
+k = 0
+
 # Ikkunan luonti
 root = tk.Tk()
 
@@ -12,7 +16,7 @@ label = Label(root, text="Pekan selain", font=("Courier 22 bold"))
 label.pack()
 
 # Osoitteen syöttökenttä
-entry= Entry(root, width= 40)
+entry = Entry(root, width= 40)
 entry.focus_set()
 entry.pack()
 
@@ -21,20 +25,29 @@ frame = HtmlFrame(root, horizontal_scrollbar="auto")
 lf = HtmlFrame(root, horizontal_scrollbar="auto")
 
 # Yhdistää internettiin ja näyttää renderöidyn html:n
-def internet():
+def internet(kw):
     # Tarvitaan osoitekentän syöte ja yhtys htmlframeen
     global entry
     global frame
     global lf
+    global k
     # Osoitteen hakeminen
-    string= entry.get()
+    string = entry.get()
     # Poistetaan aikaisempi sivu
     frame.destroy()
     # Uuden sivun luonti ja internettiin yhdistäminen sivun html:n hakemista varten
     frame = HtmlFrame(root, horizontal_scrollbar="auto")
     frame.set_content("<html></html>")
     # Tätä muuttujaa voidaan käyttää myös linkkien saamisessa
-    html = urllib.request.urlopen(string).read()
+    try:
+        html = urllib.request.urlopen(string).read()
+        # Jutut sivun vaihdosta varten (siis siinä on käytännössä sivu lista missä mennään edestakas)
+        if kw:
+            if string not in es:
+                es.append(string)
+                k = len(es) - 1
+    except:
+        html = ""    
     frame.set_content(html.decode())
     frame.pack()
     # Koko seuraava osio on: Linkkien haku, eli etsitään beaoutiful soupin avulla kaikki linkit, joita sivulla on jotta pääsee kunnolla surffaamaaan verkossa kopiomalla sivun sisällä olevan linkin ja liittämällä sen kenttään ja painamalla avaa
@@ -45,7 +58,7 @@ def internet():
         hrefs.append(a['href'])
     #print(hrefs)
     for x in hrefs:
-        thtml += "        <h1>" + x + "</h1>\n"
+        thtml += "        <h2>" + x + "</h2>\n"
     thtml += "    </body>\n</html>"    
     # Poistetaan aikaisempi sivu
     lf.destroy()
@@ -55,10 +68,35 @@ def internet():
     lf.set_content(thtml)
     lf.pack()      
         
+# Mennään edellieen
+def sw():
+    global es, k
+    if k - 1 >= 0:
+        k -= 1
+        entry.delete(0, END)
+        print(es, k)
+        entry.insert(0, es[k])
+        internet(False)
 
+# Mennään "tulevaan"
+def swt():
+    global es, k
+    if k + 1 < len(es):
+        k += 1
+        entry.delete(0, END)
+        entry.insert(0, es[k]) 
+        internet(True)       
+
+# Ei voi pistää arvoja suoraan command =, kautta
+def inter():
+    internet(True)
 
 # Avaa nappi, joka suorittaa internet funktion
-tk.Button(root, text= "Avaa", width= 20, command = internet).pack(pady=20)
+tk.Button(root, text= "Avaa", width= 20, command = inter).pack(pady=20)
+
+# Napit sivuun palaamiseen
+tk.Button(root, text= "←", width= 2, font=("Helvetica 22 bold"), command = sw).pack(side=LEFT, padx=50)
+tk.Button(root, text= "→", width= 2, font=("Helvetica 22 bold"), command = swt).pack(side=RIGHT, padx=50)
 
 # Avataan ikkuna
 root.mainloop()
